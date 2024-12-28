9bd2c43f8a28ec06a2b871697ba0b54d3b893740
newtechlearne <sujayc211@gmail.com>
2024-12-28 17:20:48 +0530

    Add project files.

---
+++ Controllers/HomeController1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project.Models; // Assuming the Model class is in this namespace
+
+namespace Project.Controllers
+{
+    public class HomeController : Controller // Renamed to HomeController
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var items = new List<Model>
+            {
+                new Model { Id = 1, Name = "Item 1", Price = 10.99M },
+                new Model { Id = 2, Name = "Item 2", Price = 20.99M },
+                new Model { Id = 3, Name = "Item 3", Price = 30.99M }
+            };
+
+            return View(items); // Passes the items to the view
+        }
+    }
+}
+
+
---
+++ Models/Model.cs
@@ -0,0 +1,11 @@
+﻿namespace Project.Models
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
+
